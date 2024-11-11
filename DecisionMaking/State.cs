a840c55d6b6a8294cce8ad1350622dfebaa629f0
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-11-11 00:23:07 -0400

    State using reflection

--- DecisionMaking/State.cs
+++ DecisionMaking/State.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 
+[Serializable]
 public class State
 {
 	public string stateName;
-	public List<Action> stateActions = new List<Action>();
+	public List<MonoBehaviour> stateBehaviours = new List<MonoBehaviour>();
 	public List<Transition> transitions = new List<Transition>();
+
+	private Dictionary<MonoBehaviour, object> savedProperties = new Dictionary<MonoBehaviour, object>();
 
 	public void EnterState()
 	{
 		// Load any actions assigned to this state
 		// and execute what needs to be done when entering the state
-		foreach (Action action in stateActions)
+		foreach (MonoBehaviour behaviour in stateBehaviours)
 		{
-			action.Load();
-			action.OnStateEnter();
-			(action as MonoBehaviour).enabled = true;
+			if (behaviour != null)
+			{
+				LoadProperties(behaviour);
+				behaviour.enabled = true;
+			}
 		}
 	}
 
@@ -23,11 +30,44 @@ public class State
 	{
 		// Execute what needs to be done when exiting the state and
 		// save the state of current actions
-		foreach (Action action in stateActions)
+		foreach (MonoBehaviour behaviour in stateBehaviours)
 		{
-			action.Save();
-			action.OnStateExit();
-			(action as MonoBehaviour).enabled = false;
+			if (behaviour != null)
+			{
+				behaviour.enabled = false;
+				SaveProperties(behaviour);
+			}
+		}
+	}
+
+	public void SaveProperties(MonoBehaviour behaviour)
+	{
+		var type = behaviour.GetType();
+		var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+		var propertyValues = new Dictionary<string, object>();
+		foreach (var field in fields)
+		{
+			propertyValues[field.Name] = field.GetValue(behaviour);
+		}
+		savedProperties[behaviour] = propertyValues;
+	}
+
+	public void LoadProperties(MonoBehaviour behaviour)
+	{
+		if (savedProperties.ContainsKey(behaviour))
+		{
+			var type = behaviour.GetType();
+			var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var propertyValues = savedProperties[behaviour] as Dictionary<string, object>;
+			foreach (var field in fields)
+			{
+				if (propertyValues.ContainsKey(field.Name))
+				{
+					field.SetValue(behaviour, propertyValues[field.Name]);
+				}
+			}
 		}
 	}
 }
